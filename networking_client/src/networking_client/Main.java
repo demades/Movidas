d9a26b20dbd2740c662e5a67ac06e8ea6e7c476e
Fernando <fernando.navarro@champ.aero>
2020-02-03 20:35:41 +0100

    Simple example. Enable provide a URL and get file downloaded as 'file'

--- networking_client/src/networking_client/Main.java
+++ networking_client/src/networking_client/Main.java
@@ -1,27 +1,28 @@
+/**
+ * https://www.admfactory.com/socket-example-in-java/
+ */
 package networking_client;
+import java.util.Scanner;
+
 
 import java.net.*;  
 import java.io.*;  
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, IOException {
-		Socket s= new Socket("www.google.com",443);  
-		DataInputStream din=new DataInputStream(s.getInputStream());  
-		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+	public static void main(String[] args) throws IOException, Exception {
+        String url = new String();
+		Scanner inUser = new Scanner(System.in);
+		ServerSocket server = new ServerSocket(5000);
+		Socket c = new Socket("localhost",4000);  
+		ObjectInputStream input = new ObjectInputStream(c.getInputStream());
+		ObjectOutputStream output = new ObjectOutputStream(c.getOutputStream());
+		System.out.println("Introduce your URL");
+		String text = inUser.nextLine();
+		output.writeObject("GET:" + text);
+        
+		Thread.sleep(10000);
 		  
-		String str="",str2="";  
-		while(!str.equals("stop")){  
-		str=br.readLine();  
-		dout.writeUTF(str);  
-		dout.flush();  
-		str2=din.readUTF();  
-		System.out.println("Server says: "+str2);  
-		}  
-		  
-		dout.close();  
-		s.close();  
 	}
 
 }
